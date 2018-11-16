85f0eb03efb37997b244d8d6e27a86283d0d72ec
Pavel Smolin <isingwithaz@gmail.com>
2018-11-16 14:18:26 +0300

    Create utils file

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-const fs = require('fs');
-const bmp = require('bmp-js');
+const { getRGBfromBMP, prepareBMP } = require('./utils');
 
 const imageSrc = process.argv[2]; 
-
-const prepareBMP = src => {
-    const bmpBuffer = fs.readFileSync(src);
-    const bmpData = bmp.decode(bmpBuffer);
-    const imageBuffer = bmpData.data;
-
-    return imageBuffer.toJSON().data;
-}
-
-const getRGBfromBMP = inputArr => {
-    const outputArr = [];
-    inputArr.forEach((pixel, index) => {
-        const currentIndex = Math.floor(index/4) ;
-        if ((index) % 4 !== 0) {
-            outputArr[currentIndex] = outputArr[currentIndex] ? `${outputArr[currentIndex]} ${pixel}` : pixel;
-        }
-    });
-
-    return outputArr;
-}
 
 console.log(getRGBfromBMP(prepareBMP(imageSrc)));
--- utils.js
+++ utils.js
@@ -1,1 +1,27 @@
-// utils will be back soon...
+const fs = require('fs');
+const bmp = require('bmp-js');
+
+const prepareBMP = src => {
+    const bmpBuffer = fs.readFileSync(src);
+    const bmpData = bmp.decode(bmpBuffer);
+    const imageBuffer = bmpData.data;
+
+    return imageBuffer.toJSON().data;
+}
+
+const getRGBfromBMP = inputArr => {
+    const outputArr = [];
+    inputArr.forEach((pixel, index) => {
+        const currentIndex = Math.floor(index/4) ;
+        if ((index) % 4 !== 0) {
+            outputArr[currentIndex] = outputArr[currentIndex] ? `${outputArr[currentIndex]} ${pixel}` : pixel;
+        }
+    });
+
+    return outputArr;
+}
+
+module.exports = {
+    prepareBMP,
+    getRGBfromBMP
+}
